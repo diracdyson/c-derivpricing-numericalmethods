ab3f557b8bfb909654c653a89c6bd81e8e09f620
Brandonlee Santos <64438740+diracdyson@users.noreply.github.com>
2022-11-15 10:37:00 -0500

    Add files via upload

---
+++ blackscholes/stuff.h
@@ -0,0 +1,17 @@
+#ifndef stuff_h
+#define stuff_h
+
+using namespace std;
+#include<vector>
+typedef vector<double> SamplePath;
+
+class BSModel{
+	public:
+		double S0, r, sigma;
+		BSModel(double S0_, double r_, double sigma_)
+		{S0=S0_; r=r_; sigma=sigma_; srand(time(NULL));}
+		void GenerateSamplePath(double T, int m, SamplePath& S);
+
+
+};
+#endif
